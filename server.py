654b28d664c07fa9212b911f78db052029c135d6
Matt Pedersen <matthewnpedersen@gmail.com>
2017-01-10 15:09:27 -0800

    init

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/survey', methods=['POST'])
+def new_survey():
+  print "hello there"
+  # name, location, fav_lang, comment = request.form
+
+
+app.run(debug=True)
