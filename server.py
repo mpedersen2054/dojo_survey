f1808f318f271c5883a30c85627f4ef1349e6e1b
Matt Pedersen <matthewnpedersen@gmail.com>
2017-01-10 16:05:22 -0800

    wire up the data from the form request into a dictionary, and show it in /result

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+
+# populate data from form request in here
+form_info = {}
 
 @app.route('/')
 def index():
@@ -8,12 +11,17 @@ def index():
 
 @app.route('/survey', methods=['POST'])
 def new_survey():
-  print "hello there"
-  # name, location, fav_lang, comment = request.form
+  # print "hello there"
+  form_info['name'] = request.form['name']
+  form_info['location'] = request.form['location']
+  form_info['favlang'] = request.form['favlang']
+  form_info['comment'] = request.form['comment']
+  return redirect('/result')
 
 @app.route('/result')
 def result():
-  return render_template('result.html')
+  print form_info
+  return render_template('result.html', data=form_info)
 
 
 app.run(debug=True)
