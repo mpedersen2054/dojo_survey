cfb9f9f15fe879625241dd19967db381ae1e7be4
Matt Pedersen <matthewnpedersen@gmail.com>
2017-01-11 20:53:30 -0800

    remove form_data and use session instead

--- server.py
+++ server.py
@@ -1,21 +1,30 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = 'asecretkey'
 
 # populate data from form request in here
 form_info = {}
 
 @app.route('/')
 def index():
+  if not 'name' in session:
+    session['name'] = ''
+  if not 'location' in session:
+    session['location'] = ''
+  if not 'favlang' in session:
+    session['favlang'] = ''
+  if not 'comment' in session:
+    session['comment'] = ''
   return render_template('index.html')
 
 @app.route('/survey', methods=['POST'])
 def new_survey():
   # print "hello there"
-  form_info['name'] = request.form['name']
-  form_info['location'] = request.form['location']
-  form_info['favlang'] = request.form['favlang']
-  form_info['comment'] = request.form['comment']
+  session['name']     = request.form['name']
+  session['location'] = request.form['location']
+  session['favlang']  = request.form['favlang']
+  session['comment']  = request.form['comment']
   return redirect('/result')
 
 @app.route('/result')
